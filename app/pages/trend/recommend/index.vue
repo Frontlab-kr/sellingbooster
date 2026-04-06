995554ea39a2d89ee59421da7b76b952d02064cc
eldestbro <123484134+Frontlab-kr@users.noreply.github.com>
2026-04-06 10:05:10 +0900

    Update index.vue

--- app/pages/trend/recommend/index.vue
+++ app/pages/trend/recommend/index.vue
@@ -352,7 +352,7 @@ const top30 = ref([
 const categoryTop30 = ref([
   {
     rank: 1,
-    imgSrc: '/temp/top1.png',
+    imgSrc: 'https://picsum.photos/200/300',
     brand: '스텐리1913',
     mall: '네이버쇼핑',
     title: '스텐리 스텐리1913 젠처 H2.0 플로우스테이트 텀블러',
@@ -360,7 +360,7 @@ const categoryTop30 = ref([
   },
   {
     rank: 2,
-    imgSrc: '/temp/top2.png',
+    imgSrc: 'https://picsum.photos/200/300',
     brand: '스텐리1913',
     mall: '네이버쇼핑',
     title: '스텐리 스텐리1913 젠처 프로듀어 플립 스트로 텀블러 887ml 크림',
@@ -368,7 +368,7 @@ const categoryTop30 = ref([
   },
   {
     rank: 3,
-    imgSrc: '/temp/top3.png',
+    imgSrc: 'https://picsum.photos/200/300',
     brand: '락앤락',
     mall: '네이버쇼핑',
     title:
@@ -377,7 +377,7 @@ const categoryTop30 = ref([
   },
   {
     rank: 4,
-    imgSrc: '/temp/top4.png',
+    imgSrc: 'https://picsum.photos/200/300',
     brand: '스텐리1913',
     mall: '네이버쇼핑',
     title: '스텐리 퀜처 플로우 스테이트 텀블러 H2.0',
@@ -385,7 +385,7 @@ const categoryTop30 = ref([
   },
   {
     rank: 5,
-    imgSrc: '/temp/top5.png',
+    imgSrc: 'https://picsum.photos/200/300',
     brand: '스텐리1913',
     mall: '네이버쇼핑',
     title: '스텐리 바이탈라이즈 쉐이커 텀블러 591ml + 컨테이너 103ml 세트',
@@ -393,7 +393,7 @@ const categoryTop30 = ref([
   },
   {
     rank: 6,
-    imgSrc: '/temp/top6.png',
+    imgSrc: 'https://picsum.photos/200/300',
     brand: '보아르',
     mall: '네이버쇼핑',
     title: '보아르 투웨이 대용량 손잡이 스텐 보온 보냉 텀블러...',
@@ -401,7 +401,7 @@ const categoryTop30 = ref([
   },
   {
     rank: 7,
-    imgSrc: '/temp/top7.png',
+    imgSrc: 'https://picsum.photos/200/300',
     brand: '락앤락',
     mall: '네이버쇼핑',
     title: '락앤락 모그 스텐 머그컵 텀블러',
@@ -410,7 +410,7 @@ const categoryTop30 = ref([
   {
     id: 'stanley-aerolight-33',
     rank: 8,
-    imgSrc: '/temp/top8.png',
+    imgSrc: 'https://picsum.photos/200/300',
     brand: '스텐리',
     mall: '네이버쇼핑',
     title: '스텐리 아이스플로우 에어로라이트 패스트플로우 텀블러',
@@ -419,7 +419,7 @@ const categoryTop30 = ref([
   },
   {
     rank: 9,
-    imgSrc: '/temp/top9.png',
+    imgSrc: 'https://picsum.photos/200/300',
     brand: '락앤락',
     mall: '네이버쇼핑',
     title: '락앤락 메트로카페 세라믹 텀블러 바닐라라떼 650ml',
