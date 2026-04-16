bb7ab39fe3ed6b859109033f606e21e0d360d14d
eldestbro <123484134+Frontlab-kr@users.noreply.github.com>
2026-04-16 19:01:20 +0900

    1

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -91,11 +91,13 @@
           <div class="sb-dashboard-card-head">
             <h5>오늘(2026.04.06)의 플래너 체크하세요.</h5>
             <div class="sb-dashboard-card-head-menu">
-              <Button variant="text" severity="white">
-                <template #icon>
-                  <IconArrowUpRight class="ico-arrow-up-right" />
-                </template>
-              </Button>
+              <NuxtLink to="/">
+                <Button variant="text" severity="white">
+                  <template #icon>
+                    <IconArrowUpRight class="ico-arrow-up-right" />
+                  </template>
+                </Button>
+              </NuxtLink>
               <SbMenu :items="menuItems" />
             </div>
           </div>
@@ -136,11 +138,13 @@
                 <div class="sb-dashboard-card-head-menu__text">
                   전일 00시 기준
                 </div>
-                <Button variant="text" severity="white">
-                  <template #icon>
-                    <IconArrowUpRight class="ico-arrow-up-right" />
-                  </template>
-                </Button>
+                <NuxtLink to="/">
+                  <Button variant="text" severity="white">
+                    <template #icon>
+                      <IconArrowUpRight class="ico-arrow-up-right" />
+                    </template>
+                  </Button>
+                </NuxtLink>
                 <SbMenu :items="menuItems" />
               </div>
             </div>
@@ -269,11 +273,13 @@
           <div class="sb-dashboard-card-head">
             <h5>판매 속도를 높이는 AI의 가격 제안</h5>
             <div class="sb-dashboard-card-head-menu">
-              <Button variant="text" severity="white">
-                <template #icon>
-                  <IconArrowUpRight class="ico-arrow-up-right" />
-                </template>
-              </Button>
+              <NuxtLink to="/">
+                <Button variant="text" severity="white">
+                  <template #icon>
+                    <IconArrowUpRight class="ico-arrow-up-right" />
+                  </template>
+                </Button>
+              </NuxtLink>
               <SbMenu :items="menuItems" />
             </div>
           </div>
@@ -324,21 +330,140 @@
         </div>
       </div>
       <div class="col-12">
-        <div class="sb-dashboard-card">
-          <div class="sb-dashboard-card-head">
-            <h5>내 상품 카테고리 급상승 키워드</h5>
-            <div class="sb-dashboard-card-head-menu">
-              <div class="sb-dashboard-card-head-menu__text">최근 7일 기준</div>
-              <SbMenu :items="menuItems" />
+        <div class="sb-dashboard-trend">
+          <div class="sb-dashboard-card">
+            <div class="sb-dashboard-card-head">
+              <h5>내 상품 카테고리 급상승 키워드</h5>
+              <div class="sb-dashboard-card-head-menu">
+                <div class="sb-dashboard-card-head-menu__text">
+                  최근 7일 기준
+                </div>
+                <SbMenu :items="menuItems" />
+              </div>
             </div>
-          </div>
-          <div class="sb-dashboard-card-body">
-            <div class="sb-tab">
-              <Button label="건강기능식품" variant="text" class="active" />
-              <Button label="이너뷰티" variant="text" />
-              <Button label="코스메틱" variant="text" />
-              <Button label="패션" variant="text" />
-              <Button label="펫" variant="text" />
+            <div class="sb-dashboard-card-body">
+              <div class="sb-tab">
+                <Button label="건강기능식품" variant="text" class="active" />
+                <Button label="이너뷰티" variant="text" />
+                <Button label="코스메틱" variant="text" />
+                <Button label="패션" variant="text" />
+                <Button label="펫" variant="text" />
+              </div>
+              <div class="sb-dashboard-trend-contents">
+                <div class="sb-dashboard-trend-table">
+                  <div class="sb-table">
+                    <DataTable
+                      v-scroll-end
+                      :value="trendKeyword"
+                      responsiveLayout="scroll"
+                      scrollable
+                    >
+                      <Column
+                        field="keyword"
+                        header="급상승 키워드"
+                        bodyClass="text-left"
+                        style="min-width: 346px"
+                      >
+                        <template #body="slotProps">
+                          <span>{{ slotProps.data.keyword }}</span>
+                        </template>
+                      </Column>
+
+                      <Column
+                        field="growthRate"
+                        header="상승률"
+                        bodyClass="text-left"
+                        style="min-width: 346px"
+                      >
+                        <template #body="slotProps">
+                          <div class="sb-table-body-progress">
+                            <div class="sb-table-body-progress__title">
+                              {{ slotProps.data.growthRate }}%
+                            </div>
+                            <div class="sb-table-body-progress__contents">
+                              <ProgressBar
+                                :value="slotProps.data.growthRate"
+                              ></ProgressBar>
+                            </div>
+                          </div>
+                        </template>
+                      </Column>
+
+                      <Column
+                        field="reason"
+                        header="상승 사유 요약"
+                        bodyClass="text-left"
+                        style="min-width: 346px"
+                      >
+                        <template #body="slotProps">
+                          <span>{{ slotProps.data.reason }}</span>
+                        </template>
+                      </Column>
+
+                      <template #empty>
+                        <div class="sb-nodata">
+                          <IconIllustrationSmile
+                            class="ico-illustration-smile"
+                          />
+                          <div class="sb-nodata__text">
+                            <p>분석된 급상승 키워드가 없습니다.</p>
+                          </div>
+                        </div>
+                      </template>
+                    </DataTable>
+                  </div>
+                </div>
+                <div class="sb-dashboard-trend-product">
+                  <div class="sb-dashboard-trend-product-head">
+                    <strong>시너지를 높여줄 추천 상품</strong>
+                    <NuxtLink to="/">
+                      <Button variant="text">
+                        <span class="p-button-label"
+                          >내 상품 랭킹 확인하기</span
+                        >
+                        <IconArrowSmallRight class="ico-arrow-small-right" />
+                      </Button>
+                    </NuxtLink>
+                  </div>
+                  <div class="sb-dashboard-trend-product-list">
+                    <div class="sb-dashboard-trend-product-list-scroll">
+                      <NuxtLink
+                        to="/"
+                        class="sb-dashboard-trend-product-list-item"
+                        v-for="n in 20"
+                        :key="n"
+                      >
+                        <div
+                          class="sb-dashboard-trend-product-list-item__thumb"
+                        >
+                          <img src="https://picsum.photos/200/300" alt="" />
+                        </div>
+                        <div
+                          class="sb-dashboard-trend-product-list-item__contents"
+                        >
+                          <div
+                            class="sb-dashboard-trend-product-list-item__category"
+                          >
+                            <span>뉴트리코어</span>
+                            <span>11번가</span>
+                          </div>
+                          <div
+                            class="sb-dashboard-trend-product-list-item__title"
+                          >
+                            하이퍼셀 오메가3 식물성 알티지(RTG오메가 3)하이퍼셀
+                            오메가3 식물성 알티지(RTG오메가 3)
+                          </div>
+                          <div
+                            class="sb-dashboard-trend-product-list-item__price"
+                          >
+                            16,000원
+                          </div>
+                        </div>
+                      </NuxtLink>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -352,11 +477,13 @@
                 <div class="sb-dashboard-card-head-menu__text">
                   정보 출처 K-snapp
                 </div>
-                <Button variant="text" severity="white">
-                  <template #icon>
-                    <IconArrowUpRight class="ico-arrow-up-right" />
-                  </template>
-                </Button>
+                <NuxtLink to="/">
+                  <Button variant="text" severity="white">
+                    <template #icon>
+                      <IconArrowUpRight class="ico-arrow-up-right" />
+                    </template>
+                  </Button>
+                </NuxtLink>
                 <SbMenu :items="menuItems" />
               </div>
             </div>
@@ -404,11 +531,13 @@
                 <div class="sb-dashboard-card-head-menu__text">
                   발행: 셀링부스터
                 </div>
-                <Button variant="text" severity="white">
-                  <template #icon>
-                    <IconArrowUpRight class="ico-arrow-up-right" />
-                  </template>
-                </Button>
+                <NuxtLink to="/">
+                  <Button variant="text" severity="white">
+                    <template #icon>
+                      <IconArrowUpRight class="ico-arrow-up-right" />
+                    </template>
+                  </Button>
+                </NuxtLink>
                 <SbMenu :items="menuItems" />
               </div>
             </div>
@@ -457,11 +586,13 @@
           <div class="sb-dashboard-card-head">
             <h5>관심 키워드</h5>
             <div class="sb-dashboard-card-head-menu">
-              <Button variant="text" severity="white">
-                <template #icon>
-                  <IconArrowUpRight class="ico-arrow-up-right" />
-                </template>
-              </Button>
+              <NuxtLink to="/">
+                <Button variant="text" severity="white">
+                  <template #icon>
+                    <IconArrowUpRight class="ico-arrow-up-right" />
+                  </template>
+                </Button>
+              </NuxtLink>
               <SbMenu :items="menuItems" />
             </div>
           </div>
@@ -554,11 +685,13 @@
               <div class="sb-dashboard-card-head-menu__text">
                 네이버 쇼핑 기준
               </div>
-              <Button variant="text" severity="white">
-                <template #icon>
-                  <IconArrowUpRight class="ico-arrow-up-right" />
-                </template>
-              </Button>
+              <NuxtLink to="/">
+                <Button variant="text" severity="white">
+                  <template #icon>
+                    <IconArrowUpRight class="ico-arrow-up-right" />
+                  </template>
+                </Button>
+              </NuxtLink>
               <SbMenu :items="menuItems" />
             </div>
           </div>
@@ -837,7 +970,58 @@ const keywordTop10 = ref([
     competition: 0.02,
   },
 ]);
-
+const trendKeyword = ref([
+  {
+    keyword: '혈당컷',
+    growthRate: 84,
+    reason: '중장년 다이어트 관심 증가',
+  },
+  {
+    keyword: '마그네슘 글라시네이트',
+    growthRate: 59,
+    reason: '수면/근육 이슈 콘텐츠 확산',
+  },
+  {
+    keyword: 'NMN',
+    growthRate: 52,
+    reason: '항노화 트렌드 재부상',
+  },
+  {
+    keyword: '관절 MSM',
+    growthRate: 34,
+    reason: '환절기 관절 키워드 증가',
+  },
+  {
+    keyword: '프로바이오틱스',
+    growthRate: 31,
+    reason: '장 건강 이슈 지속',
+  },
+  {
+    keyword: '혈당컷',
+    growthRate: 84,
+    reason: '중장년 다이어트 관심 증가',
+  },
+  {
+    keyword: '마그네슘 글라시네이트',
+    growthRate: 59,
+    reason: '수면/근육 이슈 콘텐츠 확산',
+  },
+  {
+    keyword: 'NMN',
+    growthRate: 52,
+    reason: '항노화 트렌드 재부상',
+  },
+  {
+    keyword: '관절 MSM',
+    growthRate: 34,
+    reason: '환절기 관절 키워드 증가',
+  },
+  {
+    keyword: '프로바이오틱스',
+    growthRate: 31,
+    reason: '장 건강 이슈 지속',
+  },
+]);
 const ksnappItems = [
   {
     rank: 1,
