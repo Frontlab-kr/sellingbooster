bd6ee99a52a5cc240dc1906f0abf1aac97b0522d
eldestbro <123484134+Frontlab-kr@users.noreply.github.com>
2026-04-17 16:11:23 +0900

    Update index.vue

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -477,6 +477,80 @@
           </div>
         </div>
       </div>
+      <div class="col-12 sb-dashboard-report">
+        <div class="sb-dashboard-card">
+          <div class="sb-dashboard-card-head">
+            <h5>채널별 페이스 리포트</h5>
+            <div class="sb-tab-circle">
+              <Button label="네이버쇼핑" variant="text" class="active" />
+              <Button label="쿠팡" variant="text" />
+              <Button label="11번가" variant="text" />
+              <Button label="옥션" variant="text" />
+            </div>
+          </div>
+          <div class="sb-dashboard-card-body">
+            <div class="sb-dashboard-report-contents">
+              <div class="sb-dashboard-report-info">
+                <div class="sb-dashboard-report-info-item">
+                  <div class="sb-dashboard-report-info-item__title">
+                    <strong>주문</strong>
+                    <Button variant="text">
+                      <span class="p-button-label">26.01.14 15:38 기준</span>
+                      <IconSystemRotateRight class="ico-system-rotate-right" />
+                    </Button>
+                  </div>
+                  <div class="sb-dashboard-report-info-item__text">
+                    <h3>12,356</h3>
+                    <p>13,123,300원</p>
+                  </div>
+                </div>
+                <div class="sb-dashboard-report-info-item">
+                  <div class="sb-dashboard-report-info-item__title">
+                    <strong>반품</strong>
+                    <Button variant="text">
+                      <span class="p-button-label">26.01.14 15:38 기준</span>
+                      <IconSystemRotateRight class="ico-system-rotate-right" />
+                    </Button>
+                  </div>
+                  <div class="sb-dashboard-report-info-item__text">
+                    <h3>6</h3>
+                    <p>325,300원</p>
+                  </div>
+                </div>
+              </div>
+              <div class="sb-dashboard-report-chart">
+                <div class="sb-dashboard-report-chart-head">
+                  <Select
+                    ref="selectRef"
+                    v-model="selectedSort"
+                    :options="selectedSortOption"
+                    optionLabel="name"
+                    size="small"
+                    class="p-select-text"
+                    panelClass="p-select-overlay--text"
+                    :pt="{
+                      overlay: {
+                        onclick: (event) => {
+                          if (event.target === event.currentTarget) {
+                            selectRef.hide();
+                          }
+                        },
+                      },
+                    }"
+                  />
+                  <div class="sb-dashboard-report-chart-label">
+                    <p class="color-primary">주문 건수</p>
+                    <p class="color-secondary">주문 금액</p>
+                  </div>
+                </div>
+                <div class="sb-dashboard-report-chart-body">
+                  <SbChartBarDashboard :chart-data="trendData" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="sb-dashboard-banner">
         <div class="sb-dashboard-banner__text">
           <h6>
@@ -742,80 +816,6 @@
                     </div>
                   </div>
                 </NuxtLink>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 sb-dashboard-report">
-        <div class="sb-dashboard-card">
-          <div class="sb-dashboard-card-head">
-            <h5>채널별 페이스 리포트</h5>
-            <div class="sb-tab-circle">
-              <Button label="네이버쇼핑" variant="text" class="active" />
-              <Button label="쿠팡" variant="text" />
-              <Button label="11번가" variant="text" />
-              <Button label="옥션" variant="text" />
-            </div>
-          </div>
-          <div class="sb-dashboard-card-body">
-            <div class="sb-dashboard-report-contents">
-              <div class="sb-dashboard-report-info">
-                <div class="sb-dashboard-report-info-item">
-                  <div class="sb-dashboard-report-info-item__title">
-                    <strong>주문</strong>
-                    <Button variant="text">
-                      <span class="p-button-label">26.01.14 15:38 기준</span>
-                      <IconSystemRotateRight class="ico-system-rotate-right" />
-                    </Button>
-                  </div>
-                  <div class="sb-dashboard-report-info-item__text">
-                    <h3>12,356</h3>
-                    <p>13,123,300원</p>
-                  </div>
-                </div>
-                <div class="sb-dashboard-report-info-item">
-                  <div class="sb-dashboard-report-info-item__title">
-                    <strong>반품</strong>
-                    <Button variant="text">
-                      <span class="p-button-label">26.01.14 15:38 기준</span>
-                      <IconSystemRotateRight class="ico-system-rotate-right" />
-                    </Button>
-                  </div>
-                  <div class="sb-dashboard-report-info-item__text">
-                    <h3>6</h3>
-                    <p>325,300원</p>
-                  </div>
-                </div>
-              </div>
-              <div class="sb-dashboard-report-chart">
-                <div class="sb-dashboard-report-chart-head">
-                  <Select
-                    ref="selectRef"
-                    v-model="selectedSort"
-                    :options="selectedSortOption"
-                    optionLabel="name"
-                    size="small"
-                    class="p-select-text"
-                    panelClass="p-select-overlay--text"
-                    :pt="{
-                      overlay: {
-                        onclick: (event) => {
-                          if (event.target === event.currentTarget) {
-                            selectRef.hide();
-                          }
-                        },
-                      },
-                    }"
-                  />
-                  <div class="sb-dashboard-report-chart-label">
-                    <p class="color-primary">주문 건수</p>
-                    <p class="color-secondary">주문 금액</p>
-                  </div>
-                </div>
-                <div class="sb-dashboard-report-chart-body">
-                  <SbChartBarDashboard :chart-data="trendData" />
-                </div>
               </div>
             </div>
           </div>
