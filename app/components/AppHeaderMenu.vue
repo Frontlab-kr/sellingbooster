9eed04adcb1a260a4f191d0441ea1c2a759cfead
eldestbro <123484134+Frontlab-kr@users.noreply.github.com>
2026-04-29 08:05:26 +0900

    1

--- app/components/AppHeaderMenu.vue
+++ app/components/AppHeaderMenu.vue
@@ -148,7 +148,7 @@
           </div>
         </div>
       </div>
-      <!-- <div class="sb-header-login">
+      <div class="sb-header-login">
         <NuxtLink to="/auth/login/index02">
           <Button rounded severity="white">
             <template #icon>
@@ -156,7 +156,7 @@
             </template>
           </Button>
         </NuxtLink>
-      </div> -->
+      </div>
       <div class="sb-header-user" :class="{ active: isUserOpen }">
         <button class="sb-header-user-button" @click="toggleLayer('user')">
           <div class="sb-header-user__thumb">
